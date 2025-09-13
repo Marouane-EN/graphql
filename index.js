ec9f8ef44bdac21814cb9097a0c27de38191e813
Marouane EN <173193132+Marouane-EN@users.noreply.github.com>
2025-09-13 17:15:18 +0100

    Refactor login function to include GraphQL transaction fetch and sum calculation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let username
 let password
+let sum = 0
 const btn = document.getElementById('btn')
 btn.addEventListener('click', login)
 
@@ -22,30 +23,39 @@ function login() {
 
         console.log(data)
     })
+    console.log('== = =', localStorage.getItem("jwt"));
 
-    // fetch("https://learn.zone01oujda.ma//api/graphql-engine/v1/graphql",
-    //     {
-    //         method: "POST",
-    //         headers: {
-    //             "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
-    //             "Content-Type": "application/json"
-    //         },
+    fetch("https://learn.zone01oujda.ma//api/graphql-engine/v1/graphql",
+        {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("jwt")}`,
+                "Content-Type": "application/json"
+            },
 
-    //         body: {
-    //             query: `
-    //                 user {
-    //                     id
-    //                     login
-    //                 }
-                
-    //             `
-    //         }
+            body: JSON.stringify({
+                query: `
+                query {
+                    transaction(where:{type: {_eq:"xp"}, event: { object: { name: { _eq: "Module" } } }}, order_by: {createdAt:asc}) {
+    
+    type
+    amount
+  }
+                }
+                `
+            })
 
-    //     }
+        }
 
-    // ).then(res => res.json()).then(data => {
-    //     console.log(data)
-    // })
+    ).then(res => res.json()).then(data => {
+        data.data.transaction.forEach(element => {
+            console.log(element.amount);
+            
+            sum += element.amount
+        });
+        console.log("====  ", data)
+        console.log("sum",sum);
+    })
 
 }
 
